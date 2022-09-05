f9f13cf79056aa42186bbba1f03900d4f5885c52
Kristina36916 <kristina.ivancuk@gmail.com>
2022-09-05 03:53:30 +0200

    inicijalizovan kostur

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Proizvodi from "./Components/Proizvodi";
+import Meni from "./Components/Meni";
+import Postovi from "./Components/Postovi";
+import Kontakt from "./Components/Kontakt";
+import React from 'react'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Meni> </Meni>
+      <Proizvodi></Proizvodi>
+      <Postovi></Postovi>
+      <Kontakt></Kontakt>
     </div>
+    
   );
 }
 
